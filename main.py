3c224dfbd0db9572717ec1e12b872ff06d994de3
Kamilake <exjang0@gmail.com>
2024-01-26 17:10:25 +0900

    현재 값과 새로운 값이 다른 경우에만 입력 필드 업데이트

--- main.py
+++ main.py
@@ -27,23 +27,28 @@ time.sleep(페이지_지연_시간)
 while True:
   # 입력 필드 찾기
   try:
-      input_field = driver.find_element(By.XPATH, '//input[@placeholder="Paste file name here"]')
+    input_field = driver.find_element(By.XPATH, '//input[@placeholder="Paste file name here"]')
   except:
-      button = driver.find_element(By.XPATH, '//button[text()="Where am i?"]')
-      button.click()
+    button = driver.find_element(By.XPATH, '//button[text()="Where am i?"]')
+    button.click()
 
-      input_field = driver.find_element(By.XPATH,'//input[@placeholder="Paste file name here"]')
+    input_field = driver.find_element(By.XPATH,'//input[@placeholder="Paste file name here"]')
 
-  # 최신 스크린샷 파일 이름 붙여넣기
+  # 최신 스크린샷 파일 이름 가져오기
   folder_path = 스크린샷_저장_경로
   files = os.listdir(folder_path)
   paths = [os.path.join(folder_path, basename) for basename in files]
   if len(paths) > 0:
     latest_file = max(paths, key=os.path.getmtime)
     latest_file_name = os.path.basename(latest_file)
-    input_field.send_keys(latest_file_name)
-    print ("붙여넣기 완료")
-  else :
+
+    # 현재 값과 새로운 값이 다른 경우에만 입력 필드 업데이트
+    current_value = input_field.get_attribute('value')
+    if current_value != latest_file_name:
+      input_field.clear()
+      input_field.send_keys(latest_file_name)
+      print ("붙여넣기 완료")
+  else:
     input_field.clear()
 
   # 파일_변경_확인_주기 초마다 반복
